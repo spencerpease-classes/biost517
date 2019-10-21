ef7950147a591c8ab243a6cb7d744ba3db46790f
Spencer Pease <spencer.p.816@gmail.com>
2019-10-21 00:21:22 -0700

    [hw03] Create function for "difference in means" two-sample test

--- homework/hw03/functions/population_estimates.R
+++ homework/hw03/functions/population_estimates.R
@@ -13,3 +13,46 @@ confidence_interval <- function(x, alpha = .95) {
   c("lower" = point_estimate - interval, "upper" = point_estimate + interval)
 
 }
+
+two_sample_ci <- function(data, test_col = NULL, alpha = .95) {
+
+
+  # Validate parameters -----------------------------------------------------
+
+
+  # Split data --------------------------------------------------------------
+
+  split_data <- data %>%
+    dplyr::select(dplyr::group_cols(), {{test_col}}) %>%
+    dplyr::group_split(keep = FALSE) %>%
+    purrr::map(tibble::deframe) %>%
+    purrr::map(na.omit)
+
+
+  # Calculate statistics ----------------------------------------------------
+
+  z_score <- qnorm((alpha + 1) / 2)
+  point_est <- abs(mean(split_data[[1]]) - mean(split_data[[2]]))
+
+  std_deviation <- split_data %>%
+    purrr::map(~(var(.) / length(.))) %>%
+    purrr::reduce(`+`) %>%
+    sqrt()
+
+  interval <- z_score * std_deviation
+  ci_lower <- point_est - interval
+  ci_upper <- point_est + interval
+
+
+  # Organize output ---------------------------------------------------------
+
+  ci_lower_name <- paste0(alpha * 100, "% CI (lower)")
+  ci_upper_name <- paste0(alpha * 100, "% CI (upper)")
+
+  tibble::tibble(
+    `Point est.` = point_est,
+    !!ci_lower_name := ci_lower,
+    !!ci_upper_name := ci_upper
+  )
+
+}
